c829c48a262d8f3b5c790a004b39cfeac2d1226e
isadora <isadoraalmeidapaixao@gmail.com>
2025-08-27 19:29:11 -0300

    Create Animal.cs

---
+++ Animal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petshop2
+
+
+{
+
+    public abstract class Animal
+
+    {
+        public string nome;
+
+
+        private int idade { get; set; }
+
+
+        public int novaIdade
+        {
+            get { return idade; }
+            set
+            {
+                novaIdade = value;
+                {
+                    if (value >= 0)
+                    {
+                        Console.WriteLine("Idade inválida.");
+                    }
+                    else
+                    {
+                        idade = value;
+                    }
+                }
+            }
+        }
+
+        public Animal(string nome, int idade)
+
+        {
+            this.nome = nome;
+
+
+            this.idade = idade;
+
+        }
+
+        public virtual void Atender()
+        {
+            Console.WriteLine($"Atendendo {nome}");
+        }
+    }
+
+
+}
